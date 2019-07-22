2c81c2faba7d3d83ab31531ce07053de90855d5c
heekyeom <khk2870@gmail.com>
2019-07-22 15:24:39 +0900

    set connectors with http, https

--- src/main/java/com/project/tutorial/SpringboottutorialApplication.java
+++ src/main/java/com/project/tutorial/SpringboottutorialApplication.java
@@ -1,13 +1,41 @@
 package com.project.tutorial;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringboottutorialApplication {
 
 	public static void main(String[] args) {
+		System.out.println("start spring boot tutorial");
 		SpringApplication.run(SpringboottutorialApplication.class, args);
 	}
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+    @Bean
+    public ServletWebServerFactory serveltContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8081);
+        return connector;
+    }
+
+
 
 }
